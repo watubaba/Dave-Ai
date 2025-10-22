2973e69446c80e72c45c3270d9fc78a1e2eaa67c
gifteddevsmd <babydave081@gmail.com>
2025-10-22 19:58:55 +0300

    D

--- set.js
+++
@@ -1,7 +0,0 @@
-
-const appname = process.env.APP_NAME || '';
-const herokuapi = process.env.HEROKU_API;
-const antidel = process.env.ANTIDELETE || 'TRUE';
-
-
-module.exports = { antidel, herokuapi, appname};  
